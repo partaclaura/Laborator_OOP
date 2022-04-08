ed272773db222a349c9d8363db4a57f4777a6eee
partaclaura <laurapartac02@gmail.com>
2022-04-08 11:07:56 +0300

    removed int

--- Laborator7/Ex1.cpp
+++ Laborator7/Ex1.cpp
@@ -1,11 +1,11 @@
 
 #include<iostream>
 using namespace std;
-float operator"" _Kelvin(unsigned long long int x)
+float operator"" _Kelvin(unsigned long long x)
 {
 	return x - 273.15;
 }
-float operator"" _Fahrenheit(unsigned long long int x)
+float operator"" _Fahrenheit(unsigned long long x)
 {
 	return (x - 32) / 1.8;
 }
