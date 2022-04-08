672511b3e1c7db45f7d95a6da92c504e0e84971b
partaclaura <laurapartac02@gmail.com>
2022-04-08 11:00:44 +0300

    added ex1

---
+++ Laborator6/Ex1.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+float operator"" _Kelvin(unsigned long long int x)
+{
+	return x - 273.15;
+}
+float operator"" _Fahrenheit(unsigned long long int x)
+{
+	return (x - 32) / 1.8;
+}
+int main()
+{
+	float a = 300_Kelvin;
+	float b = 120_Fahrenheit;
+	cout << "300 K = " << a << " C";
+	cout << "\n120 F = " << b << " C";
+	return 0;
+}
